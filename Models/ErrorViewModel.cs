0342ae67753e1e4e23d9400f17b3126e11a8d51f
Pamela Pragides <ppragides1@my.bcit.ca>
2023-12-04 12:43:23 -0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sips.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
