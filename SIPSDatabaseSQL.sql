474a03866d63e980e6261bc6736c30d310bbceef
Everest Shi <shi.everest@gmail.com>
2023-12-08 10:44:31 -0800

    updated erd and db

--- SIPSDatabaseSQL.sql
+++ SIPSDatabaseSQL.sql
@@ -4,6 +4,7 @@ DROP TABLE IF EXISTS OrderDetail;
 DROP TABLE IF EXISTS Item;
 DROP TABLE IF EXISTS ItemSize;
 DROP TABLE IF EXISTS [Transaction];
+DROP TABLE IF EXISTS [OrderStatus];
 DROP TABLE IF EXISTS Rating;
 DROP TABLE IF EXISTS Contact;
 DROP TABLE IF EXISTS [Store];
@@ -31,41 +32,43 @@ CREATE TABLE Contact ( pkUserID INTEGER CONSTRAINT Contact_PK PRIMARY KEY AUTOIN
 						, birthDate     		DATETIME		
 						, isDrinkRedeemed		VARCHAR(10) 	NOT NULL
 						, fkUserTypeID			INTEGER			NOT NULL
-						, fkStoreID				INTEGER			NOT NULL
 						, CONSTRAINT ContactCredentialFK FOREIGN KEY (fkUserTypeID) REFERENCES Credential(pkUserTypeID) ON UPDATE CASCADE ON DELETE CASCADE
-						, CONSTRAINT ContactStoreFK FOREIGN KEY (fkStoreID) REFERENCES [Store](pkStoreID) ON UPDATE CASCADE ON DELETE CASCADE
 						, CONSTRAINT CustomerContact UNIQUE (email));
 						
 						
 CREATE TABLE Rating ( pkRatingID INTEGER CONSTRAINT Rating_PK PRIMARY KEY AUTOINCREMENT
-						, rating				VARCHAR(5) 		NOT NULL
+						, [rating]				VARCHAR(5) 		NOT NULL
 						, [date]				DATETIME		NOT NULL
 						, comment				VARCHAR(255)	NOT NULL
 						, fkStoreID				INTEGER			NOT NULL
 						, fkUserID				INTEGER			NOT NULL
 						, CONSTRAINT RatingStoreFK FOREIGN KEY (fkStoreID) REFERENCES [Store](pkStoreID) ON UPDATE CASCADE ON DELETE CASCADE
 						, CONSTRAINT RatingContactFK FOREIGN KEY (fkUserID) REFERENCES Contact(pkUserID) ON UPDATE CASCADE ON DELETE CASCADE);
-						
+
+
+CREATE TABLE [OrderStatus] ( pkStatusID INTEGER CONSTRAINT OrderStatus_PK PRIMARY KEY AUTOINCREMENT
+							, isOrdered 		VARCHAR(10)		NOT NULL
+							, isPickedUp		VARCHAR(10)		NOT NULL);
 						
 CREATE TABLE [Transaction] ( pkTransactionID INTEGER CONSTRAINT Transaction_PK PRIMARY KEY AUTOINCREMENT
-								, totalPrice 	DECIMAL(10, 2)	NOT NULL
-								, timeOrdered	VARCHAR(30)		NOT NULL
 								, dateOrdered	DATETIME 		NOT NULL
-								, totalItems	INTEGER			NOT NULL
-								, isOrdered		VARCHAR(10)		NOT NULL
-								, isPickedUp	VARCHAR(10)		NOT NULL
 								, fkStoreID		INTEGER			NOT NULL
-								, CONSTRAINT TransactionStoreFK FOREIGN KEY (fkStoreID) REFERENCES [Store](pkStoreID) ON UPDATE CASCADE ON DELETE CASCADE);
+								, fkUserID 		INTEGER			NOT NULL
+								, fkStatusID	INTEGER			NOT NULL
+								, CONSTRAINT TransactionStoreFK FOREIGN KEY (fkStoreID) REFERENCES [Store](pkStoreID) ON UPDATE CASCADE ON DELETE CASCADE
+								, CONSTRAINT TransactionContactFK FOREIGN KEY (fkUserID) REFERENCES Contact(pkUserID) ON UPDATE CASCADE ON DELETE CASCADE
+								, CONSTRAINT TransactionOrderStatusFK FOREIGN KEY (fkStatusID) REFERENCES OrderStatus(pkStatusID) ON UPDATE CASCADE ON DELETE CASCADE);
 								
 		
 CREATE TABLE Item ( pkItemID INTEGER CONSTRAINT Item_PK PRIMARY KEY AUTOINCREMENT
 						, name					VARCHAR(255)	NOT NULL
-						, image					BLOB			NOT NULL
+						, image					BLOB			
 						, description			VARCHAR(255)	NOT NULL
 						, ice					VARCHAR(10)		NOT NULL
 						, sweetness				VARCHAR(10)		NOT NULL
 						, basePrice				DECIMAL(10, 2)	NOT NULL
-						, isBirthdayDrink		VARCHAR(10)		NOT NULL
+						, inventory				INTEGER			NOT NULL
+						, itemType				VARCHAR(30)		NOT NULL
 						, CONSTRAINT UniqueName UNIQUE (name));
 						
 						
@@ -76,13 +79,15 @@ CREATE TABLE ItemSize ( pkSizeID INTEGER CONSTRAINT Size_PK PRIMARY KEY AUTOINCR
 						
 CREATE TABLE AddIn ( pkAddInID INTEGER CONSTRAINT AddIn_PK PRIMARY KEY AUTOINCREMENT
 						, addInName				VARCHAR(30)		NOT NULL
-						, priceModifier			DECIMAL(10, 2)	NOT NULL);
+						, priceModifier			DECIMAL(10, 2)	NOT NULL
+						, image					BLOB			);
 				
 				
 CREATE TABLE OrderDetail ( pkOrderDetailID INTEGER CONSTRAINT OrderDetail_PK PRIMARY KEY AUTOINCREMENT
 							, price				DECIMAL(10, 2) 	NOT NULL
 							, quantity			INTEGER			NOT NULL
-							, totalPrice		DECIMAL(10, 2)	NOT NULL
+							, isBirthdayDrink	VARCHAR(10, 2)	NOT NULL
+							, promoValue		DECIMAL(10, 2)	NOT NULL
 							, fkItemID			INTEGER			NOT NULL
 							, fkTransactionID	INTEGER			NOT NULL
 							, fkSizeID			INTEGER			NOT NULL
