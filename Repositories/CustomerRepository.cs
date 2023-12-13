478b09a7fbb342f763f1921d6197c767db09797d
samanehesh <145700227+samanehesh@users.noreply.github.com>
2023-12-13 11:58:38 -0800

    product CRUD added

---
+++ Repositories/CustomerRepository.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using Sips.Models;
+
+namespace Sips.Repositories
+{
+    public class CustomerRepository
+    {
+        private readonly SipsContext _db;
+
+        public CustomerRepository(SipsContext db)
+        {
+            _db = db;
+        }
+        public IEnumerable<Contact> GetAll()
+        {
+            return _db.Contacts;
+        }
+        public Contact GetById(int id)
+        {
+            return _db.Contacts.FirstOrDefault(p => p.PkUserId == id);
+        }
+    }
+}
