10497d69ea39af0d228bf185d0368d0f3e397229
Everest Shi <shi.everest@gmail.com>
2024-02-12 11:46:05 -0800

    azure db set up

---
+++ SipsModels/Item.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sips.SipsModels;
+
+public partial class Item
+{
+    public int ItemId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Description { get; set; } = null!;
+
+    public decimal BasePrice { get; set; }
+
+    public int Inventory { get; set; }
+
+    public string? UrlString { get; set; }
+
+    public int? SweetnessId { get; set; }
+
+    public int? IceId { get; set; }
+
+    public int? ItemTypeId { get; set; }
+
+    public virtual Ice? Ice { get; set; }
+
+    public virtual ItemType? ItemType { get; set; }
+
+    public virtual ICollection<OrderDetail> OrderDetails { get; set; } = new List<OrderDetail>();
+
+    public virtual Sweetness? Sweetness { get; set; }
+}
---
+++ SipsModels/OrderStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sips.SipsModels;
+
+public partial class OrderStatus
+{
+    public int StatusId { get; set; }
+
+    public bool IsOrdered { get; set; }
+
+    public bool IsPickedUp { get; set; }
+
+    public virtual ICollection<Transaction> Transactions { get; set; } = new List<Transaction>();
+}
