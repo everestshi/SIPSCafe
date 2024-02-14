bfb19838a379f542a1e795cb4f98f001e45b8df4
Everest Shi <shi.everest@gmail.com>
2024-02-14 11:46:14 -0800

    changed types

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,19 +6,32 @@ namespace Sips.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly SipsdatabaseContext _context;
+        private readonly SipsdatabaseContext _db;
         private readonly ILogger<HomeController> _logger;
         private readonly IConfiguration _configuration;
 
-        public HomeController(SipsdatabaseContext context, ILogger<HomeController> logger, IConfiguration configuration)
+        public HomeController(SipsdatabaseContext db, ILogger<HomeController> logger, IConfiguration configuration)
         {
-            _context = context;
+            _db = db;
             _logger = logger;
             _configuration = configuration;
         }
 
         public IActionResult Index()
         {
+            string userEmail = User.Identity.Name;
+            var user = _db.Contacts.Where(c => c.Email == userEmail).FirstOrDefault();
+
+            if (user != null)
+            {
+                string userFirstName = user.FirstName;
+
+                if (HttpContext.Session != null)
+                {
+                    HttpContext.Session.SetString("SessionUserName", userFirstName);
+                }
+            }
+
             var connectionString = _configuration["ConnectionStrings:DefaultConnection"];
             return View();
         }
