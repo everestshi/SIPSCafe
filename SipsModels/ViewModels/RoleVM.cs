84c404d90d319ea668b3698f50d4ae66e7e98923
sydsnow <ssnowball1@my.bcit.ca>
2024-02-14 10:57:51 -0800

    roles added, index and create working, still needs delete

---
+++ SipsModels/ViewModels/RoleVM.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sips.SipsModels.ViewModels
+{
+    public class RoleVM
+    {
+        [Display(Name = "ID")]
+        public string? Id { get; set; }
+
+        [Required]
+        [Display(Name = "Role Name")]
+        public string RoleName { get; set; }
+    }
+}
