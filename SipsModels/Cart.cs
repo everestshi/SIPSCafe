5adc92bb0df2d7c7bebb6f6a4b2043a5e47f0ff7
Pamela Pragides <ppragides1@my.bcit.ca>
2024-03-01 10:45:49 -0800

    add to cart pam

--- SipsModels/Cart.cs
+++ SipsModels/Cart.cs
@@ -3,6 +3,27 @@
     [Serializable]
     public class Cart
     {
-        public List<Item> Items { get; set; } = new List<Item>();
+        public List<Item> Items { get; set; }
+
+        public Cart()
+        {
+            Items = new List<Item>();
+        }
+
+        public void AddItem(Item item)
+        {
+            Items.Add(item);
+        }
+
+        public void RemoveItem(Item item)
+        {
+            Items.Remove(item); 
+        }
+
+        public decimal GetTotal()
+        {
+            return Items.Sum(item => item.BasePrice);
+        }
     }
+
 }
